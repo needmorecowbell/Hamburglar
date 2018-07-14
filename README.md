886a3118d5e20ee679c4027e2df4f0f3907a1b1f
Adam Musciano <amusciano@gmail.com>
2018-07-13 20:59:56 -0400

    First Commit!

---
+++ README.md
@@ -0,0 +1,30 @@
+## The Hamburglar
+
+<p align="center">
+    <img src="res/hamburgler.jpg" width="400"></img>
+</p>
+
+Multithreaded and recursive directory scraping script. Stores useful information with the filepath and finding. All in one file, no external packages required! 
+
+## Installation
+
+`python3 hamburgler.py ~/Directory/Here`
+
+This will recursively scan for files, then analyzes each file for a variety of findings using regex filters
+
+## The Hamburglar can find
+
+- ipv4 addresses (public and local)
+- emails
+- private keys
+- urls
+- cryptocurrency addresses
+
+
+## Notes
+
+- Inspiration came from needmorecowbell/sniff-paste, I wanted the same regex scraping but for every file in a given directory. 
+
+- Please contribute! If there's an error let me know -- even better if you can fix it :)
+	- Regex Contributions would be very helpful, and should be pretty easy to add!
+- Please don't use this project maliciously, it is meant to be an analysis tool
---
+++ hamburglar.py
@@ -0,0 +1,81 @@
+import re
+import os
+import sys
+import threading
+
+rootdir = sys.argv[1]
+fileStack= []
+regexList= {
+    "ipv4":"[0-9]+(?:\.[0-9]+){3}",
+    "https-site":"https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+",
+    "http-site":"http?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+",
+    "phones":"\(?\b[2-9][0-9]{2}\)?[-. ]?[2-9][0-9]{2}[-. ]?[0-9]{4}\b",
+    "emails":"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+",
+    "Slack Token": "(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",
+    "RSA private key": "-----BEGIN RSA PRIVATE KEY-----",
+    "SSH (OPENSSH) private key": "-----BEGIN OPENSSH PRIVATE KEY-----",
+    "SSH (DSA) private key": "-----BEGIN DSA PRIVATE KEY-----",
+    "SSH (EC) private key": "-----BEGIN EC PRIVATE KEY-----",
+    "PGP private key block": "-----BEGIN PGP PRIVATE KEY BLOCK-----",
+    "Facebook Oauth": "[f|F][a|A][c|C][e|E][b|B][o|O][o|O][k|K].*['|\"][0-9a-f]{32}['|\"]",
+    "Twitter Oauth": "[t|T][w|W][i|I][t|T][t|T][e|E][r|R].*['|\"][0-9a-zA-Z]{35,44}['|\"]",
+    "GitHub": "[g|G][i|I][t|T][h|H][u|U][b|B].*[['|\"]0-9a-zA-Z]{35,40}['|\"]",
+    "Google Oauth": "(\"client_secret\":\"[a-zA-Z0-9-_]{24}\")",
+    "AWS API Key": "AKIA[0-9A-Z]{16}",
+    "Heroku API Key": "[h|H][e|E][r|R][o|O][k|K][u|U].*[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}",
+    "Generic Secret": "[s|S][e|E][c|C][r|R][e|E][t|T].*['|\"][0-9a-zA-Z]{32,45}['|\"]",
+    "bitcoin-address" : "[13][a-km-zA-HJ-NP-Z1-9]{25,34}" ,
+    "bitcoin-uri" : "bitcoin:([13][a-km-zA-HJ-NP-Z1-9]{25,34})" ,
+    "bitcoin-xpub-key" : "(xpub[a-km-zA-HJ-NP-Z1-9]{100,108})(\\?c=\\d*&h=bip\\d{2,3})?" ,
+    "monero-address": "(?:^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93}$)",
+    "ethereum-address": "(?:^0x[a-fA-F0-9]{40}$)",
+    "litecoin-address":"(?:^[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}$)",
+    "bitcoin-cash-address":"(?:^[13][a-km-zA-HJ-NP-Z1-9]{33}$)",
+    "dash-address":"(?:^X[1-9A-HJ-NP-Za-km-z]{33}$)",
+    "ripple-address":"(?:^r[0-9a-zA-Z]{33}$)",
+    "neo-address":"(?:^A[0-9a-zA-Z]{33}$)",
+    "dogecoin-address":"(?:^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32}$)"
+}
+
+
+#iterate through every file in given directory
+def scan():
+    for root, subFolders, files in os.walk(rootdir):
+        for folder in subFolders:
+            for entry in files:
+                filePath = os.path.join(root,entry)
+                fileStack.append(filePath)
+                print(filePath)
+
+
+def _file_read():
+    while(fileStack):
+        filePath= fileStack.pop()
+        try:
+            with open(filePath, "r") as scanFile:
+                #print("WORKER CALLED: "+filePath+"\n")
+                results = _sniff_text(filePath,scanFile.read().replace('\n', ''))
+                if (len(results.items())>1): print("Results: \n"+ str(results)+"\n")
+                #for line in scanFile:
+                    #filePath=filePath
+                    #print(line)
+        except Exception as e:
+            print("Error: "+str(e))
+
+def _sniff_text(filepath, text):
+    results= {"filepath":filepath} 
+    for key, value in regexList.items():
+        findings= re.findall(value, text)
+        if(len(findings)>0):
+            results.update({key:findings})
+    return results
+    
+scan()
+print("Scan Complete")
+# workers to handle fileStack
+for x in range(5):
+    t = threading.Thread(target=_file_read)
+    #t.setDaemon(True)
+    t.start()
+
+print("Snooping complete")

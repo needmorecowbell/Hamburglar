bc0dee1ab8b46051efcfe538a859336c3234eade
Adam Musciano <amusciano@gmail.com>
2018-07-15 08:45:52 -0400

    alphabetized regex list

--- hamburglar.py
+++ hamburglar.py
@@ -36,34 +36,34 @@ blacklist = [
 
 # Regex dictionary, comment out a line to stop checking for entry, and add a line for new filters
 regexList= {
-    "ipv4":"[0-9]+(?:\.[0-9]+){3}",
-    "site":"https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+",
-#    "phone":"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$",
-    "email":"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+",
-    "Slack Token": "(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",
-    "RSA private key": "-----BEGIN RSA PRIVATE KEY-----",
-    "SSH (OPENSSH) private key": "-----BEGIN OPENSSH PRIVATE KEY-----",
-    "SSH (DSA) private key": "-----BEGIN DSA PRIVATE KEY-----",
-    "SSH (EC) private key": "-----BEGIN EC PRIVATE KEY-----",
-    "PGP private key block": "-----BEGIN PGP PRIVATE KEY BLOCK-----",
-    "Facebook Oauth": "[f|F][a|A][c|C][e|E][b|B][o|O][o|O][k|K].*['|\"][0-9a-f]{32}['|\"]",
-    "Twitter Oauth": "[t|T][w|W][i|I][t|T][t|T][e|E][r|R].*['|\"][0-9a-zA-Z]{35,44}['|\"]",
-    "GitHub": "[g|G][i|I][t|T][h|H][u|U][b|B].*[['|\"]0-9a-zA-Z]{35,40}['|\"]",
-    "Google Oauth": "(\"client_secret\":\"[a-zA-Z0-9-_]{24}\")",
     "AWS API Key": "AKIA[0-9A-Z]{16}",
-    "Heroku API Key": "[h|H][e|E][r|R][o|O][k|K][u|U].*[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}",
-    "Generic Secret": "[s|S][e|E][c|C][r|R][e|E][t|T].*['|\"][0-9a-zA-Z]{32,45}['|\"]",
-#FIND BETTER REGEX    "bitcoin-address" : "[13][a-km-zA-HJ-NP-Z1-9]{25,34}" ,
+#    "bitcoin-address" : "[13][a-km-zA-HJ-NP-Z1-9]{25,34}" ,
+    "bitcoin-cash-address":"(?:^[13][a-km-zA-HJ-NP-Z1-9]{33})",
     "bitcoin-uri" : "bitcoin:([13][a-km-zA-HJ-NP-Z1-9]{25,34})" ,
     "bitcoin-xpub-key" : "(xpub[a-km-zA-HJ-NP-Z1-9]{100,108})(\\?c=\\d*&h=bip\\d{2,3})?" ,
-    "monero-address": "(?:^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93})",
-    "ethereum-address": "(?:^0x[a-fA-F0-9]{40})",
-    "litecoin-address":"(?:^[LM3][a-km-zA-HJ-NP-Z1-9]{26,33})",
-    "bitcoin-cash-address":"(?:^[13][a-km-zA-HJ-NP-Z1-9]{33})",
     "dash-address":"(?:^X[1-9A-HJ-NP-Za-km-z]{33})",
-    "ripple-address":"(?:^r[0-9a-zA-Z]{33})",
+    "dogecoin-address":"(?:^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32})",
+    "email":"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+",
+    "ethereum-address": "(?:^0x[a-fA-F0-9]{40})",
+    "Facebook Oauth": "[f|F][a|A][c|C][e|E][b|B][o|O][o|O][k|K].*['|\"][0-9a-f]{32}['|\"]",
+    "Generic Secret": "[s|S][e|E][c|C][r|R][e|E][t|T].*['|\"][0-9a-zA-Z]{32,45}['|\"]",
+    "GitHub": "[g|G][i|I][t|T][h|H][u|U][b|B].*[['|\"]0-9a-zA-Z]{35,40}['|\"]",
+    "Google Oauth": "(\"client_secret\":\"[a-zA-Z0-9-_]{24}\")",
+    "Heroku API Key": "[h|H][e|E][r|R][o|O][k|K][u|U].*[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}",
+    "ipv4":"[0-9]+(?:\.[0-9]+){3}",
+    "litecoin-address":"(?:^[LM3][a-km-zA-HJ-NP-Z1-9]{26,33})",
+    "monero-address": "(?:^4[0-9AB][1-9A-HJ-NP-Za-km-z]{93})",
     "neo-address":"(?:^A[0-9a-zA-Z]{33})",
-    "dogecoin-address":"(?:^D{1}[5-9A-HJ-NP-U]{1}[1-9A-HJ-NP-Za-km-z]{32})"
+    "PGP private key block": "-----BEGIN PGP PRIVATE KEY BLOCK-----",
+    "phone":"^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$",
+    "ripple-address":"(?:^r[0-9a-zA-Z]{33})",
+    "RSA private key": "-----BEGIN RSA PRIVATE KEY-----",
+    "site":"https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+",
+    "Slack Token": "(xox[p|b|o|a]-[0-9]{12}-[0-9]{12}-[0-9]{12}-[a-z0-9]{32})",
+    "SSH (DSA) private key": "-----BEGIN DSA PRIVATE KEY-----",
+    "SSH (EC) private key": "-----BEGIN EC PRIVATE KEY-----",
+    "SSH (OPENSSH) private key": "-----BEGIN OPENSSH PRIVATE KEY-----",
+    "Twitter Oauth": "[t|T][w|W][i|I][t|T][t|T][e|E][r|R].*['|\"][0-9a-zA-Z]{35,44}['|\"]"
 }
 
 #Get First Argument (file or directory)
@@ -84,7 +84,7 @@ def scan():
 
                 filepath= os.path.join(root,entry)
 
-                if whitelistOn): 
+                if(whitelistOn):
                     #if whitelisted, check if entry is valid and add to stack
                     if _iswhitelisted(filepath):
                         print("[+] whitelist finding: "+str(filepath))
@@ -92,7 +92,7 @@ def scan():
                     else:
                         #if its not, forget about the file
                         break
-                elif _isfiltered(filepath): 
+                elif _isfiltered(filepath):
                     #if whitelist is off, check blacklist
                             print("[-] "+filepath+" blacklisted, not scanning")
                             break
@@ -126,7 +126,7 @@ def scan():
                         except Exception as e:
                             print("[-] read error: "+str(e) )
 
-    else: 
+    else:
         #we just have a single file, so add it to the stack
         print("[+] single file passed")
         filestack.add(passedPath)
@@ -168,13 +168,13 @@ def _file_read():
 def _sniff_text(filePath, text):
     """ checks every regex for findings, and return a dictionary of all findings """
     results= {}
-    for key, value in regexList.items(): 
+    for key, value in regexList.items():
         findings= set(re.findall(value, text))
         if findings:
             results.update({key:findings})
     return results
 
-def displayCumulative(): 
+def displayCumulative():
     """ Displays finding report """
     print(json.dumps(dict(cumulativeFindings), default=lambda x: str(x), sort_keys=True, indent=4))
 
